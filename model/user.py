302893640b4ac5fd780705be1947b3db415ac2e2
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2023-03-24 00:33:47 +0100

    user entites created using orm

--- model/user.py
+++ model/user.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-from flask import Column, String
-from sqlalchemy.orm import relationship
+from model import basic
+from flask import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class User(Base):
+Base = declarative_base()
+
+
+class User(basic, Base):
         __tablename__ = 'users'
-        id = Column(db.Integer, primary_key=True)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-
-
-
-
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        address = Column(String(256), nullable=False)
+        phone = Column(String(128), nullable=False)
+        gender = Column(String(128), nullable=False)
+        dob_day = Column(Integer(128), nullable=False)
+        dob_month = Column(Integer(128), nullable=False)
+        dob_year = Column(Integer(128), nullable=False)
