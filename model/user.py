29f0ed27dde7917dda84df676345ec11758b6af3
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2023-03-24 01:19:14 +0100

    user module updated

--- model/user.py
+++ model/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from model import basic
+from basic import Basic
 from flask import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,15 +7,18 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class User(basic, Base):
-        __tablename__ = 'users'
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        address = Column(String(256), nullable=False)
-        phone = Column(String(128), nullable=False)
-        gender = Column(String(128), nullable=False)
-        dob_day = Column(Integer(128), nullable=False)
-        dob_month = Column(Integer(128), nullable=False)
-        dob_year = Column(Integer(128), nullable=False)
+class User(Basic, Base):
+    __tablename__ = 'users'
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    address = Column(String(256), nullable=False)
+    phone = Column(String(128), nullable=False)
+    gender = Column(String(128), nullable=False)
+    dob_day = Column(Integer(128), nullable=False)
+    dob_month = Column(Integer(128), nullable=False)
+    dob_year = Column(Integer(128), nullable=False)
+
+clss  = User()
+print(clss)
