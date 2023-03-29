e75c2b0baa8b4e48c69b2dae705b74af766fd1a7
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2023-03-29 06:06:02 +0100

    User definition and Properties updated

--- model/user.py
+++ model/user.py
@@ -2,16 +2,23 @@
 from basic import Basic
 from flask import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
+import bcrypt
 
 
 Base = declarative_base()
 
 
 class User(Basic, Base):
+    """Define the representation/Properties of Users
+
+    Args:
+        Basic (_type_): _This class define the common properties_
+        Base (_type_): _Class that hold the OOP equivalent of SQL syntax_
+    """
     __tablename__ = 'users'
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    email = Column(String(128), nullable=False)
+    email = Column(String(128), nullable=False, unique=True)
     password = Column(String(128), nullable=False)
     address = Column(String(256), nullable=False)
     phone = Column(String(128), nullable=False)
@@ -20,5 +27,18 @@ class User(Basic, Base):
     dob_month = Column(Integer(128), nullable=False)
     dob_year = Column(Integer(128), nullable=False)
 
-clss  = User()
-print(clss)
+    def __init__(self, *args, **kwargs):
+        """initializes user.
+           Based on inherited values from the parent class
+        """
+        super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        """sets a password by first encoding to bytes.
+           Then use bcrypt hashing, for secure staorable value
+        """
+        if name == "password":
+            value = value.encode()
+            salt = bcrypt.gensalt()
+            hash_pwd = bcrypt.hashpw(value, salt)
+        self.__setattr__(name, hash_pwd)
