9768a360da20edf9af870bfad4c7e17b96dc2ec7
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2023-04-02 17:44:09 +0100

    updated basic class

--- models/basic.py
+++ models/basic.py
@@ -14,8 +14,8 @@ class Basic:
     for all other models
     """
     id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    updated_at = Column(DateTime, nullable=False, default=datetime.now())
 
     def __init__(self, *args, **kwargs):
         """ Initializes the basic class """
@@ -28,16 +28,18 @@ class Basic:
             if "id" not in kwargs:
                 self.id = str(uuid4())
             if "created_at" not in kwargs:
-                self.created_at = datetime.utcnow()
+                self.created_at = datetime.now()
             if "updated_at" not in kwargs:
-                self.updated_at = datetime.utcnow()
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """ """
-        pass
+        """string representation of class """
+        return "[{:s}] with identity number: ({:s})\n{}\n{}\n{}".format(
+               self.__class__.__name__, self.id, '*' * 75, self.__dict__,
+               '*' * 75)
 
     def save(self):
         """ """
