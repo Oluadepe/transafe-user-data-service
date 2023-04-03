c72abf7a5781335381cbbf54fc601760115b5e9f
Rotimi Adebowale Owolabi <31229565+rohteemie@users.noreply.github.com>
2023-04-03 02:58:26 +0100

    update the api endpoint
    update endpoint to get all transafe users

---
+++ api/v1/views/user.py
@@ -0,0 +1,15 @@
+""" Module of Users views 
+ """ 
+ from api.v1.views import api_endpoint
+ from flask import jsonify, request 
+ from models.user import User 
+  
+  
+ @api_endpoint.route('/users', methods=['GET'], strict_slashes=False) 
+ def get_all_user() -> str: 
+     """ GET /api/v1/users 
+     Return: 
+       - list of all transafe User object
+     """ 
+     
+     return ""
